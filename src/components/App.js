fe78821acc0bfc0a1356214238bc0e30d4907ddb
jay <nenojaafar3@gmail.com>
2018-04-09 22:44:31 -0500

    added the

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 // components
 import Home from "./Home";
@@ -7,19 +7,25 @@ import About from "./About";
 import Header from "./Header";
 import Teachers from "./Teachers";
 import Courses from "./Courses";
+import NotFound from "./NotFound";
 
 
 const App = () => (
   <Router>
     <div className="container">
       <Header/>
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={() => <About title="About"/>}/>
-      <Route path="/teachers" component={Teachers}/>
-      <Route path="/courses" component={Courses}/>
-      
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={() => <About title="About"/>}/>
+        <Route path="/teachers" component={Teachers}/>
+        <Route path="/courses" component={Courses}/>
+        <Route component={NotFound}/>
+      </Switch>
     </div>
   </Router>
 );
 
 export default App;
+
+// A route with no path will always render
+// A switch component will render the first path that match,if no match it will render path with no route.
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -5,22 +5,22 @@ import CSS from "./courses/CSS";
 import HTML from "./courses/HTML";
 import JavaScript from './courses/JavaScript';
 
-const Courses = () => (
+const Courses = ({match}) => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><NavLink to='/courses/html'>HTML</NavLink></li>
-        <li><NavLink to='/courses/css'>CSS</NavLink></li>
-        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
+        <li><NavLink to={`${match.url}/html`} >HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
       </ul>
     </div>
     
     {/* Write routes here... */}
-    <Route exact path="/courses" render={ ()=> <Redirect to="/courses/html"/>}/>
-    <Route path="/courses/html" component={HTML} />
-    <Route path="/courses/css" component={CSS} />
-    <Route path="/courses/javascript" component={JavaScript} />
+    <Route exact path={`${match.path}`} render={ ()=> <Redirect to={`${match.path}/html`}/>}/>
+    <Route path={`${match.path}/html`} component={HTML} />
+    <Route path={`${match.path}/css`} component={CSS} />
+    <Route path={`${match.url}/javascript`} component={JavaScript} />
 
   </div>
 );
@@ -28,3 +28,6 @@ const Courses = () => (
 export default Courses;
 
 // clicking the courses NavLink, will Route you to Redirrect to hmtl component
+// using match will make our routes more dynamic, such that now if we change the couress url it wont effect sub urls
+// match.url & match.path are exactliy the same, we use url with the url link and path with the route 
+
